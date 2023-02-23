0ae4e39e969d1c727702dd4c3d6ea67ad322f250
Raghad <119360813+RaghadNasman@users.noreply.github.com>
2023-02-23 11:36:30 +0300

    Hotel

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+let navBtn = document.querySelector("#nav-btn");
+let navList = document.querySelector(".reponsive-nav");
+navBtn.addEventListener("click", function(){
+    navList.classList.toggle("show")
+});
